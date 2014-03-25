8e9af4eea499924b143914f1c17760e77c80107a
Tony Gregerson <tony.gregerson@gmail.com>
2014-03-24 23:14:19 -0500

    PSR now defers most functions to superclass.

--- LC3Sim/src/lc3sim/core/ProcessorStatusRegister.java
+++ LC3Sim/src/lc3sim/core/ProcessorStatusRegister.java
@@ -8,39 +8,20 @@ public class ProcessorStatusRegister extends Register {
   public static final int kPBit = 0;
   
   public ProcessorStatusRegister() {
-    super(kNumBits, InputId.DontCare, InputId.PsrLoad, OutputId.Psr);
-  }
-  
-  public void Init() {
-    q_ = BitWord.Zeroes(kNumBits).SetBit(kZBit, true);
-    d_ = q_;
-    UpdateOutput(OutputId.Psr);
+    super(kNumBits, InputId.Psr, InputId.PsrLoad, OutputId.Psr);
   }
   
   public void Notify(BitWord data, OutputId sender, InputId receiver,
                      Object arg) {
-    if (sender == OutputId.External) {
-      // Asynchronous set command. Immediately update.
-      q_ = data.Resize(16, false);
-      UpdateOutput(out_id_);
-    } else {
-      switch (sender) {
-        case NzpLogic:
-          assert data.num_bits() > kNBit;
-          d_ = d_.SetBit(kNBit, data.TestBit(kNBit)).
-                  SetBit(kZBit, data.TestBit(kZBit)).
-                  SetBit(kPBit, data.TestBit(kPBit));
-          int set_count = 0;
-          if (d_.TestBit(kNBit)) set_count++;
-          if (d_.TestBit(kZBit)) set_count++;
-          if (d_.TestBit(kPBit)) set_count++;
-          assert set_count == 1;
-          break;
-        // TODO: Add load enable
-        default:
-          assert false;
-      }
+    // Sanity-check updates to NZP bits before forwarding to superclass.
+    if (receiver == InputId.Psr) {
+      int set_count = 0;
+      if (data.TestBit(kNBit)) set_count++;
+      if (data.TestBit(kZBit)) set_count++;
+      if (data.TestBit(kPBit)) set_count++;
+      assert set_count == 1 : set_count;
     }
+    super.Notify(data, sender, receiver, arg);
   }
   
   public Boolean supervisor_mode() {
